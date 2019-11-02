08227db27bef246d3ee919148e1abd2c981d7306
Alex Yates <adyates1@sheffield.ac.uk>
2019-11-02 14:38:35 +0000

    Gcloud

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, function(){
+    console.log('listening on *:' + PORT);
 });
 
 io.on('connection', function(socket){
