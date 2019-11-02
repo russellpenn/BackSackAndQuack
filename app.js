a64164c5654c06d211a89aa997aa48520722d937
Alex Yates <adyates1@sheffield.ac.uk>
2019-11-02 13:10:39 +0000

    Added io require

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
