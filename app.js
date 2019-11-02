a2113fcffc17a36201cee9ccd01325e8ca429dd7
Alex Yates <adyates1@sheffield.ac.uk>
2019-11-02 13:07:28 +0000

    Added handlers

--- app.js
+++ app.js
@@ -14,4 +14,9 @@ io.on('connection', function(socket){
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.sockets.connected[socket.id].emit('chat message', msg);
+    });
 });
