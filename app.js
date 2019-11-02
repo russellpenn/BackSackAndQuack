b101bb510fd9ca8421b7cb6678f30253bbb675b0
Alex Yates <adyates1@sheffield.ac.uk>
2019-11-02 12:48:15 +0000

    Added socket.io

---
+++ app.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
